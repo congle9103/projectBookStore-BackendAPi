efcb55f2478cb4993bea9e0b3c8611c355830bdd
congle9103 <congle9103@gmail.com>
2025-10-10 20:11:07 +0700

    .

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,8 +1,38 @@
 import createError from "http-errors";
 import Customer from "../models/Customer.model";
 
-const findAll = async () => {
-  return await Customer.find();
+const findAll = async (query: any) => {
+  const {
+    page = 1,
+    limit = 5,
+    keyword = null,
+    sort_type = "desc",
+    sort_by = "updatedAt",
+  } = query;
+
+  // SORT
+  const sortObject: Record<string, 1 | -1> = {
+    [sort_by]: sort_type === "desc" ? -1 : 1,
+  };
+
+  // WHERE
+  const where: any = {};
+  if (keyword) {
+    where.$or = [
+      { username: { $regex: keyword, $options: "i" } },
+      { fullname: { $regex: keyword, $options: "i" } },
+    ];
+  }
+
+  const skip = (page - 1) * limit;
+  const customers = await Customer.find(where)
+    .skip(skip)
+    .limit(limit)
+    .sort(sortObject);
+
+  const totalRecords = await Customer.countDocuments(where);
+
+  return { customers, page, limit, totalRecords };
 };
 
 const findById = async (id: string) => {
@@ -18,7 +48,7 @@ const create = async (payload: any) => {
     username: payload.username,
     password: payload.password,
     full_name: payload.full_name,
-    email: payload.email
+    email: payload.email,
   });
 
   await newCustomer.save();
--- src/services/staff.service.ts
+++ src/services/staff.service.ts
@@ -1,8 +1,38 @@
 import createError from "http-errors";
 import Staff from "../models/Staff.model";
 
-const findAll = async () => {
-  return await Staff.find();
+const findAll = async (query: any) => {
+  const {
+    page = 1,
+    limit = 5,
+    keyword = null,
+    sort_type = "desc",
+    sort_by = "updatedAt",
+  } = query;
+
+  // SORT
+  const sortObject: Record<string, 1 | -1> = {
+    [sort_by]: sort_type === "desc" ? -1 : 1,
+  };
+
+  // WHERE
+  const where: any = {};
+  if (keyword) {
+    where.$or = [
+      { username: { $regex: keyword, $options: "i" } },
+      { fullname: { $regex: keyword, $options: "i" } },
+    ];
+  }
+
+  const skip = (page - 1) * limit;
+  const staff = await Staff.find(where)
+    .skip(skip)
+    .limit(limit)
+    .sort(sortObject);
+
+  const totalRecords = await Staff.countDocuments(where);
+
+  return { staff, page, limit, totalRecords };
 };
 
 const findById = async (id: string) => {
